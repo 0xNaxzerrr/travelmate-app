a5f4e9b9ccc976a964fdd13c79c659f0b6399aae
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 03:06:09 +0100

    Disable native screens to resolve inset issues

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,12 @@
-import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Provider } from "react-redux";
-import store from "./src/store";
-import AppNavigator from "./src/navigation/AppNavigator";
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { enableScreens } from 'react-native-screens';
+import store from './src/store';
+import AppNavigator from './src/navigation/AppNavigator';
+
+// Explicitly enable native screens
+enableScreens(false);
 
 export default function App() {
   return (
