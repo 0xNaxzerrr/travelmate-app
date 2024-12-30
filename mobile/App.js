7af91ede9883bd24612f7b975b94c7d6d791df82
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 02:34:30 +0100

    Simplify App.js configuration

--- mobile/App.js
+++ mobile/App.js
@@ -3,12 +3,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import store from './src/store';
 import AppNavigator from './src/navigation/AppNavigator';
-import { enableScreens } from 'react-native-screens';
-
-// Explicitly enable native screens with configuration
-enableScreens({
-  skipRequestAnimationFrame: true
-});
 
 export default function App() {
   return (
