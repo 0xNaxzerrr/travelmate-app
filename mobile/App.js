fe99782b9c1cc50d0f5a045fd8b77b5cbb1ba122
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 01:53:51 +0100

    Add newArchEnabled and fix topInsetsChange warning

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import store from './src/store';
 import AppNavigator from './src/navigation/AppNavigator';
+
+// Ignorer l'avertissement topInsetsChange
+LogBox.ignoreLogs(['Unsupported top level event type "topInsetsChange" dispatched']);
 
 export default function App() {
   return (
