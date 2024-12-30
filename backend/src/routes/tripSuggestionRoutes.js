e6b4562887f7a53a4bd1f7f8deb1e30cf4813650
0xNaxzerrr <naxdev75@gmail.com>
2024-12-30 10:42:40 +0100

    remove useless code

--- backend/src/routes/tripSuggestionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tripSuggestionController = require('../controllers/tripSuggestionController');
-
-router.post('/generate', tripSuggestionController.generateTripSuggestion);
-router.get('/', tripSuggestionController.getTripSuggestion);
-
-module.exports = router;
