632feb6fc506135a265e44fb7dbcf339374ce302
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 02:02:24 +0100

    Add babel configuration for Expo project

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,10 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Si vous utilisez des plugins supplémentaires, ajoutez-les ici
-      // Par exemple : 
-      // 'react-native-reanimated/plugin'
-    ]
+    plugins: []
   };
 };
