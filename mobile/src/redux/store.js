d1d53c48820c790b2cb6e1d943594a5b3ad9f36a
_Nax <74185631+0xNaxzerrr@users.noreply.github.com>
2024-12-30 10:36:46 +0100

    feat: add Redux configuration with persistence

---
+++ mobile/src/redux/store.js
@@ -0,0 +1,28 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import authReducer from './slices/authSlice';
+import tripReducer from './slices/tripSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['auth'] // Only auth will be persisted
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+export const store = configureStore({
+  reducer: {
+    auth: persistedAuthReducer,
+    trips: tripReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST']
+      }
+    })
+});
+
+export const persistor = persistStore(store);
